264d4853140cbd93466837fad5ad60e35400b8b6
GarciaNicolasEmanuel <46333200+GarciaNicolasEmanuel@users.noreply.github.com>
2019-12-11 23:44:21 -0300

    Update CuentaAhorro.java

--- src/modelo/CuentaAhorro.java
+++ src/modelo/CuentaAhorro.java
@@ -12,15 +12,6 @@ package modelo;
 public class CuentaAhorro extends CuentaCorriente {
     private double interes=0.025;
 
-    public double getInteres() {
-        return interes;
-    }
-
-    public void setInteres(double interes) {
-        this.interes = interes;
-    }
-    
-
     public CuentaAhorro(Cliente titular, int nroCuenta, double saldo, double interes) {
         super(titular, nroCuenta, saldo);
         this.interes=interes;
@@ -37,6 +28,15 @@ public class CuentaAhorro extends CuentaCorriente {
 
     public CuentaAhorro() {
     }
+    
+     public double getInteres() {
+        return interes;
+    }
+
+    public void setInteres(double interes) {
+        this.interes = interes;
+    }
+    
     public double calcularInteres(){
         return (super.getSaldo()*this.interes)+super.getSaldo();
         
